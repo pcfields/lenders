5661447877431f60e11bfb279c9a750d00c76b0e
Peter Fields <peter@motionstatic.com>
2020-07-07 19:25:55 +0100

    style index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,49 @@ import Link from 'next/link';
 import { FC } from 'react';
 
 const IndexPage: FC = () => (
-  <ul>
-    <li>
-      <Link href="/lenders/bank-of-azeroth">
-        <a>Bank of Azeroth</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/lenders/naboo-bank">
-        <a>Naboo Bank</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/lenders/middle-earth-bank">
-        <a>Middle Earth Bank</a>
-      </Link>
-    </li>
-  </ul>
+  <div>
+    <h1>Lenders</h1>
+
+    <ul>
+      <li>
+        <Link href="/lenders/[id]" as="/lenders/bank-of-azeroth">
+          <a>Bank of Azeroth</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/lenders/[id]" as="/lenders/naboo-bank">
+          <a>Naboo Bank</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/lenders/[id]" as="/lenders/middle-earth-bank">
+          <a>Middle Earth Bank</a>
+        </Link>
+      </li>
+    </ul>
+    <style jsx>{`
+      div {
+        max-width: 300px;
+        margin: 0 auto;
+      }
+
+      ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+      }
+
+      a {
+        display: block;
+        padding: 30px;
+        margin: 30px 0;
+        border: 1px solid #000;
+        text-decoration: none;
+        color: #000;
+        text-transform: uppercase;
+      }
+    `}</style>
+  </div>
 );
 
 export default IndexPage;
